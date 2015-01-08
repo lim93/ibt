19e220b1ec5005b6eb2fbc853d6adcd5284dbd40
Lukas <lukastremblau@web.de>
2015-01-08 15:04:27 +0100

    Reservierungen werden in die DB geschrieben
    Klick auf "Reservieren" führt dazu, dass die Daten in die DB-Tabelle
    geschrieben werden. Weiterleitung auf Bestätigungsseite.

---
+++ confirm.php
@@ -0,0 +1,60 @@
+﻿<!DOCTYPE html>
+
+<html lang="de">
+
+<?php
+    require_once( 'php/db_config.php'); 
+
+	$db_link = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PW, MYSQL_DB, MYSQL_PORT); 
+
+	if(mysqli_connect_errno()) { 
+		throw new Exception("Verbindungsaufbau fehlgeschlagen: " . mysqli_connect_error());
+	} 
+	if(!$db_link->set_charset("utf8")){
+		throw new Exception("Charset-Proble: " . $db_link->error);
+	} 
+	
+    $booking_no = 42;
+	
+	if ( isset($_POST['date']) and isset($_POST['time']) and isset($_POST['table_no']) and isset($_POST['persons'])
+        and isset($_POST['first_name']) and isset($_POST['last_name']) and isset($_POST['email']) and isset($_POST['phone'])
+        and $_POST['date'] != "" and $_POST['time'] != "" and $_POST['table_no'] != "" and $_POST['persons'] != ""
+        and $_POST['first_name'] !=  "" and $_POST['last_name'] != "" and $_POST['email'] != "" and $_POST['phone'] != "" ) 
+	{
+	    $date = mysqli_real_escape_string($db_link, $_POST['date']);
+	    $time = mysqli_real_escape_string($db_link, $_POST['time']);
+	    $table_no = mysqli_real_escape_string($db_link, $_POST['table_no']);
+	    $persons = mysqli_real_escape_string($db_link, $_POST['persons']);
+	    $first_name = mysqli_real_escape_string($db_link, $_POST['first_name']);
+	    $last_name = mysqli_real_escape_string($db_link, $_POST['last_name']);
+	    $email = mysqli_real_escape_string($db_link, $_POST['email']);
+	    $phone = mysqli_real_escape_string($db_link, $_POST['phone']);
+    
+	
+        $sql="INSERT INTO bookings(booking_no, date, time, table_no, persons, first_name, last_name, email, phone) 
+	          VALUES ('$booking_no', '$date', '$time', '$table_no', '$persons', '$first_name', '$last_name', '$email', '$phone')";
+    
+        if (!$db_link->query($sql)) {
+	        exit('Fehler beim Insert' . mysqli_error($db_link));
+	    }
+    } else {
+        echo "Eingabefehler";
+	}
+?>
+
+<head>
+	<title>Bestätigung</title>
+</head>
+
+<body>
+
+    <p>Ihre Reservierung wurde erfasst. Vielen Dank.</p>
+
+<?php
+    echo "Reservierungsdatum: " . $date . ", " . $time . "\n";
+	echo "Tisch-Nr: " . $table_no;
+?> 
+
+    <p><a href="http://localhost/ibt/reservierung.php" />Zurück zum Reservierungsformular</p>
+
+</body>
--- reservierung.php
+++ reservierung.php
@@ -5,26 +5,15 @@
 <?php 
     require_once( 'php/db_config.php'); 
 
-    echo "\n".MYSQL_HOST; 
-    echo "\n".MYSQL_USER; 
-    echo "\n".MYSQL_PW; 
-    echo "\n".MYSQL_DB; 
-    echo "\n".MYSQL_PORT; 
+	$db_link = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PW, MYSQL_DB, MYSQL_PORT); 
 
-    $db_link=mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PW, MYSQL_DB, MYSQL_PORT); 
-
-    if($db_link) 
-    { 
-        echo 'Verbindung aufgebaut!'; 
-        print_r($db_link); 
-    } 
-    else 
-    { 
-        exit( "Verbindungsaufbau fehlgeschlagen: " . mysqli_connect_error()); 
-    } 
-
-    mysqli_set_charset($db_link, 'utf8'); 
-?>
+	if(mysqli_connect_errno()) { 
+		exit("Verbindungsaufbau fehlgeschlagen: " . mysqli_connect_error());
+	} 
+	if(!$db_link->set_charset("utf8")){
+		exit("Charset-Proble: " . $db_link->error);
+	} 
+?> 
 
 <head>
     <meta charset="utf-8">
@@ -151,7 +140,7 @@
             <p>Hier entsteht eine Seite für die Tischreservierung in unserem Resturant.</p>
         </div>
 
-        <form name="booking_form">
+        <form name="booking_form" action="confirm.php" method="post">
 
             <div class="panel panel-default saalplan">
 
@@ -159,49 +148,49 @@
 
                 <map name="table_map" id="table_map">
 
-                    <area id="t1" alt="Bereich Tisch1" title="Tisch 1" shape="rect" coords="611,384,650,453" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 1; return false' />
+                    <area id="t1" alt="Bereich Tisch1" title="Tisch 1" shape="rect" coords="611,384,650,453" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 1; return false' />
 
-                    <area id="t2" alt="Bereich Tisch2" title="Tisch 2" shape="rect" coords="645,264,686,296" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 2; return false' />
+                    <area id="t2" alt="Bereich Tisch2" title="Tisch 2" shape="rect" coords="645,264,686,296" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 2; return false' />
 
-                    <area id="t3" alt="Bereich Tisch3" title="Tisch 3" shape="rect" coords="577,264,618,296" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 3; return false' />
+                    <area id="t3" alt="Bereich Tisch3" title="Tisch 3" shape="rect" coords="577,264,618,296" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 3; return false' />
 
-                    <area id="t4" alt="Bereich Tisch4" title="Tisch 4" shape="rect" coords="576,196,617,228" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 4; return false' />
+                    <area id="t4" alt="Bereich Tisch4" title="Tisch 4" shape="rect" coords="576,196,617,228" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 4; return false' />
 
-                    <area id="t5" alt="Bereich Tisch5" title="Tisch 5" shape="rect" coords="645,196,686,228" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 5; return false' />
+                    <area id="t5" alt="Bereich Tisch5" title="Tisch 5" shape="rect" coords="645,196,686,228" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 5; return false' />
 
-                    <area id="t6" alt="Bereich Tisch6" title="Tisch 6" shape="rect" coords="403,422,439,468" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 6; return false' />
+                    <area id="t6" alt="Bereich Tisch6" title="Tisch 6" shape="rect" coords="403,422,439,468" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 6; return false' />
 
-                    <area id="t7" alt="Bereich Tisch7" title="Tisch 7" shape="rect" coords="328,429,369,461" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 7; return false' />
+                    <area id="t7" alt="Bereich Tisch7" title="Tisch 7" shape="rect" coords="328,429,369,461" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 7; return false' />
 
-                    <area id="t8" alt="Bereich Tisch8" title="Tisch 8" shape="rect" coords="258,422,293,467" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 8; return false' />
+                    <area id="t8" alt="Bereich Tisch8" title="Tisch 8" shape="rect" coords="258,422,293,467" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 8; return false' />
 
-                    <area id="t9" alt="Bereich Tisch9" title="Tisch 9" shape="rect" coords="403,323,438,369" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 9; return false' />
+                    <area id="t9" alt="Bereich Tisch9" title="Tisch 9" shape="rect" coords="403,323,438,369" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 9; return false' />
 
-                    <area id="t10" alt="Bereich Tisch10" title="Tisch 10" shape="rect" coords="328,326,369,359" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 10; return false' />
+                    <area id="t10" alt="Bereich Tisch10" title="Tisch 10" shape="rect" coords="328,326,369,359" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 10; return false' />
 
-                    <area id="t11" alt="Bereich Tisch11" title="Tisch 11" shape="rect" coords="256,322,293,368" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 11; return false' />
+                    <area id="t11" alt="Bereich Tisch11" title="Tisch 11" shape="rect" coords="256,322,293,368" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 11; return false' />
 
-                    <area id="t12" alt="Bereich Tisch12" title="Tisch 12" shape="rect" coords="366,132,407,165" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 12; return false' />
+                    <area id="t12" alt="Bereich Tisch12" title="Tisch 12" shape="rect" coords="366,132,407,165" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 12; return false' />
 
-                    <area id="t13" alt="Bereich Tisch13" title="Tisch 13" shape="rect" coords="267,132,308,165" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 13; return false' />
+                    <area id="t13" alt="Bereich Tisch13" title="Tisch 13" shape="rect" coords="267,132,308,165" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 13; return false' />
 
-                    <area id="t14" alt="Bereich Tisch14" title="Tisch 14" shape="rect" coords="366,44,407,77" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 14; return false' />
+                    <area id="t14" alt="Bereich Tisch14" title="Tisch 14" shape="rect" coords="366,44,407,77" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 14; return false' />
 
-                    <area id="t15" alt="Bereich Tisch15" title="Tisch 15" shape="rect" coords="268,36,304,82" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 15; return false' />
+                    <area id="t15" alt="Bereich Tisch15" title="Tisch 15" shape="rect" coords="268,36,304,82" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 15; return false' />
 
-                    <area id="t16" alt="Bereich Tisch16" title="Tisch 16" shape="rect" coords="147,379,182,443" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 16; return false' />
+                    <area id="t16" alt="Bereich Tisch16" title="Tisch 16" shape="rect" coords="147,379,182,443" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 16; return false' />
 
-                    <area id="t17" alt="Bereich Tisch17" title="Tisch 17" shape="rect" coords="145,281,186,313" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 17; return false' />
+                    <area id="t17" alt="Bereich Tisch17" title="Tisch 17" shape="rect" coords="145,281,186,313" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 17; return false' />
 
-                    <area id="t18" alt="Bereich Tisch18" title="Tisch 18" shape="rect" coords="143,156,184,188" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 18; return false' />
+                    <area id="t18" alt="Bereich Tisch18" title="Tisch 18" shape="rect" coords="143,156,184,188" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 18; return false' />
 
-                    <area id="t19" alt="Bereich Tisch19" title="Tisch 19" shape="rect" coords="145,55,180,120" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 19; return false' />
+                    <area id="t19" alt="Bereich Tisch19" title="Tisch 19" shape="rect" coords="145,55,180,120" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 19; return false' />
 
-                    <area id="t20" alt="Bereich Tisch20" title="Tisch 20" shape="rect" coords="32,353,70,457" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 20; return false' />
+                    <area id="t20" alt="Bereich Tisch20" title="Tisch 20" shape="rect" coords="32,353,70,457" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 20; return false' />
 
-                    <area id="t21" alt="Bereich Tisch21" title="Tisch 21" shape="rect" coords="32,192,70,296" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 21; return false' />
+                    <area id="t21" alt="Bereich Tisch21" title="Tisch 21" shape="rect" coords="32,192,70,296" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 21; return false' />
 
-                    <area id="t22" alt="Bereich Tisch22" title="Tisch 22" shape="rect" coords="32,33,70,137" style="outline:none;" href="#" onclick='javascript:document.booking_form.tnr.value = 22; return false' />
+                    <area id="t22" alt="Bereich Tisch22" title="Tisch 22" shape="rect" coords="32,33,70,137" style="outline:none;" href="#" onclick='javascript:document.booking_form.table_no.value = 22; return false' />
 
                 </map>
 
@@ -215,24 +204,24 @@
 
                 <div class="panel-body">
 
-                    <p>Datum: <span><input  class="form-control reducedPadding" type="text" placeholder=""></input></span>
+                    <p>Datum: <span><input  class="form-control reducedPadding" type="text" name="date" placeholder=""></input></span>
                     </p>
-                    <p>Uhrzeit: <span><input  class="form-control reducedPadding" type="text" placeholder=""></input></span>
+                    <p>Uhrzeit: <span><input  class="form-control reducedPadding" type="text" name="time" placeholder=""></input></span>
                     </p>
-                    <p>Tischnummer: <span><input  class="form-control reducedPadding" type="text" name="tnr" placeholder=""></input></span>
+                    <p>Tischnummer: <span><input  class="form-control reducedPadding" type="text" name="table_no" placeholder=""></input></span>
                     </p>
-                    <p>Anzahl Personen: <span><input  class="form-control reducedPadding" type="text" placeholder=""></input></span>
+                    <p>Anzahl Personen: <span><input  class="form-control reducedPadding" type="text" name="persons" placeholder=""></input></span>
                     </p>
 
                     <hr>
 
-                    <p>Vorname: <span><input class="form-control reducedPadding" type="text" placeholder="Vorname"></input></span>
+                    <p>Vorname: <span><input class="form-control reducedPadding" type="text" name="first_name" placeholder="Vorname"></input></span>
                     </p>
-                    <p>Name: <span><input  class="form-control reducedPadding" type="text" placeholder="Name"></input></span>
+                    <p>Name: <span><input  class="form-control reducedPadding" type="text" name="last_name" placeholder="Name"></input></span>
                     </p>
-                    <p>Email: <span><input  class="form-control reducedPadding" type="text" placeholder="name@example.de"></input></span>
+                    <p>Email: <span><input  class="form-control reducedPadding" type="text" name="email" placeholder="name@example.de"></input></span>
                     </p>
-                    <p>Telefon: <span><input  class="form-control reducedPadding" type="text" placeholder="01234 567890"></input></span>
+                    <p>Telefon: <span><input  class="form-control reducedPadding" type="text" name="phone" placeholder="01234 567890"></input></span>
                     </p>
 
                     <hr>
@@ -247,7 +236,32 @@
 
     </div>
 
-
+<?php
+    // $booking_no = 42;
+	
+	// if ( isset($_POST['date']) and isset($_POST['time']) and isset($_POST['table_no']) and isset($_POST['persons'])
+        // and isset($_POST['first_name']) and isset($_POST['last_name']) and isset($_POST['email']) and isset($_POST['phone'])
+        // and $_POST['date'] != "" and $_POST['time'] != "" and $_POST['table_no'] != "" and $_POST['persons'] != ""
+        // and $_POST['first_name'] !=  "" and $_POST['last_name'] != "" and $_POST['email'] != "" and $_POST['phone'] != "" ) 
+	// {
+	    // $date = mysqli_real_escape_string($db_link, $_POST['date']);
+	    // $time = mysqli_real_escape_string($db_link, $_POST['time']);
+	    // $table_no = mysqli_real_escape_string($db_link, $_POST['table_no']);
+	    // $persons = mysqli_real_escape_string($db_link, $_POST['persons']);
+	    // $first_name = mysqli_real_escape_string($db_link, $_POST['first_name']);
+	    // $last_name = mysqli_real_escape_string($db_link, $_POST['last_name']);
+	    // $email = mysqli_real_escape_string($db_link, $_POST['email']);
+	    // $phone = mysqli_real_escape_string($db_link, $_POST['phone']);
+    
+	
+        // $sql="INSERT INTO bookings(booking_no, date, time, table_no, persons, first_name, last_name, email, phone) 
+	          // VALUES ('$booking_no', '$date', '$time', '$table_no', '$persons', '$first_name', '$last_name', '$email', '$phone')";
+    
+        // if (!mysqli_query($db_link, $sql)) {
+	        // exit('Fehler beim Insert' . mysqli_error($db_link));
+	    // }
+    // }
+?>
 
 </body>
 
